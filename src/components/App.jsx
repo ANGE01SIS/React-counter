513919b6b84d5ff08523c92d51272609e5fdbab3
APoint <andressamaniegosandoval@gmail.com>
2024-08-03 14:07:06 +0200

    Se termina de estilar y se agrega una pequeña funcionalidad

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,65 @@
 import { useState } from "react";
 
 export function App() {
-  const [count, setCount] = useState(0);
+  let [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>{count}</h1>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Add
-      </button>
-      <button
-        onClick={() => {
-          if (count !== 0) {
-            setCount(count - 1);
+      <div className="contTotalCounter">
+        <button
+          onClick={() => {
+            if (count !== 0) {
+              setCount(count - 1);
+            }
+          }}
+          className={
+            count === 0
+              ? "button button-minus button-desactive"
+              : "button button-minus button-active"
           }
-        }}
-        className={
-          count === 0
-            ? "button-minus button-desactive"
-            : "button-minus button-active"
-        }
-      >
-        Minus
-      </button>
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+          </svg>
+        </button>
+        <h1 className="countNumber">{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+          className="button button-plus"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="inputCont">
+        <input
+          type="number"
+          onChange={(e) => setCount((count = Number(e.target.value)))}
+          className="input-change-value"
+          min={0}
+        />
+      </div>
     </>
   );
 }
